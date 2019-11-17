9fd6e96b9ff72a61d6c76ec45b1d5695b1b8af18
rsibanez89 <rsibanez89@gmail.com>
2019-11-17 17:17:02 +1100

    Added top level content

---
+++ exampleSite/content/posts/top-level-post.md
@@ -0,0 +1,7 @@
+---
+title: "Top level post next to index"
+date: 2019-10-28T22:17:34+11:00
+draft: false
+---
+
+Top level post next to index
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,12 +1,50 @@
-// Tree view
 window.onload = function() {
+  
+  //--- Tree view
   var toggler = document.getElementsByClassName("caret");
   var i;
-
   for (i = 0; i < toggler.length; i++) {
     toggler[i].addEventListener("click", function() {
       this.parentElement.querySelector(".nested").classList.toggle("active");
       this.classList.toggle("caret-down");
     });
   }
+
+  //--- Dragable menu
+  var handler = document.querySelector('.navigation-bar');
+  var wrapper = handler.closest('.page-container');
+  var navigation = wrapper.querySelector('.tree-navigation');
+  var isHandlerDragging = false;
+
+  document.addEventListener('mousedown', function(e) {
+    if (e.target === handler) {
+      isHandlerDragging = true;
+    }
+  });
+
+  document.addEventListener('mousemove', function(e) {
+    if (!isHandlerDragging) {
+      return false;
+    }
+
+    // Get offset
+    var containerOffsetLeft = wrapper.offsetLeft;
+
+    // Get x-coordinate of pointer relative to container
+    var pointerRelativeXpos = e.clientX - containerOffsetLeft;
+    
+    var navigationMinWidth = 300;
+
+    // Resize navigation
+    // * 8px is the left/right spacing between .handler and its inner pseudo-element
+    // * Set flex-grow to 0 to prevent it from growing
+    navigation.style.width = (Math.max(navigationMinWidth, pointerRelativeXpos)) + 'px';
+    navigation.style.flexGrow = 0;
+  });
+
+  document.addEventListener('mouseup', function(e) {
+    // Turn off dragging flag when user mouse is up
+    isHandlerDragging = false;
+  });
+
 };
